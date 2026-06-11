a1b135aae2b0522f0e845558aa31dfb8859b54e4
Rama <ricoramiro552@gmail.com>
2026-06-11 11:07:22 -0300

    Initial commit

--- src/main/java/me/rama/Main.java
+++ src/main/java/me/rama/Main.java
@@ -1,17 +1,32 @@
 package me.rama;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+public class Main {
+
+    private static JDA jda;
+
+    public static void main(String[] args) {
+
+        log("Starting ZerstBot", true);
+        String token = System.getenv("ZERSTBOT_TOKEN");
+        if(token == null){
+            log("Token is null, check permissions or define the env variable.", true);
         }
+
+        jda = JDABuilder.createDefault(token).build();
+
+        /*
+         Sistema de votaciones (reacciones)
+         Cooldown de mensajes
+
+         Base de datos
+         */
+    }
+
+    public static void log(String message, boolean logToFile) {
+        System.out.println(message);
     }
 }
---
+++ src/main/java/me/rama/Meme.java
@@ -0,0 +1,10 @@
+package me.rama;
+
+public class Meme {
+
+    private String message_id;
+    private Member sender;
+    private int votes;
+
+
+}
