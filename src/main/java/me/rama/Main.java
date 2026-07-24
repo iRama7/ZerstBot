38827c91cc57a1784429c18b840ecf6297028498
rama <ricoramiro552@gmail.com>
2026-07-24 01:56:49 -0300

    Last commit

--- src/main/java/me/rama/Main.java
+++ src/main/java/me/rama/Main.java
@@ -1,49 +1,90 @@
 package me.rama;
 
-import com.google.gson.Gson;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvException;
+import me.rama.bot.ButtonListener;
+import me.rama.bot.Commands;
+import me.rama.bot.MessageListener;
+import me.rama.db.Database;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.components.buttons.Button;
+import net.dv8tion.jda.api.components.buttons.ButtonStyle;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.sql.SQLException;
 import java.util.EnumSet;
 
 
 public class Main {
 
-    private static JDA jda;
-    private Config config;
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(Main.class);
+
+    public static JDA jda;
+    public static Database db;
+    private static Config config;
+    public static Button upButton;
+    public static MessageListener messageListener;
+
 
     public static void main(String[] args) {
 
-        log("Starting ZerstBot...", true);
+        LOGGER.info("Starting ZerstBot...");
 
-        loadConfig();
-        String token = tryInit();
-
-        if(token != null){
-            jda = JDABuilder.createDefault(token, EnumSet.of(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT))
-                    .addEventListeners(new MessageListener())
-                    .build();
-        }else{
-            log("[Error] Token is null", true);
+        try {
+            config = Config.load(Path.of(getJarDirectory(), "config.json"));
+        } catch (IOException e) {
+            LOGGER.error("[Error] Could not load config.json {}", e.getMessage());
+            return;
         }
 
-        /*
-         Sistema de votaciones (reacciones)
-         Cooldown de mensajes
+        String token = getToken();
+        if (token == null) {
+            LOGGER.error("[Error] Token may not be null");
+        }else {
 
-         Base de datos
-         */
+            messageListener = new MessageListener(config);
+            ButtonListener buttonListener = new ButtonListener(config);
+            Commands commandsListener = new Commands(config);
+            jda = JDABuilder.createDefault(token, EnumSet.of(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT))
+                    .addEventListeners(messageListener, buttonListener, commandsListener)
+                    .disableCache(CacheFlag.VOICE_STATE, CacheFlag.EMOJI, CacheFlag.STICKER, CacheFlag.SOUNDBOARD_SOUNDS, CacheFlag.SCHEDULED_EVENTS)
+                    .build();
+            try {
+                jda.awaitReady();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
+                db = new Database(getJarDirectory() + "/database.db");
+            }catch (SQLException e){
+                LOGGER.error("Could not connect to database: {}", e.getMessage());
+            }
+
+            upButton = getUpButton();
+            buttonListener.init();
+            messageListener.init();
+            messageListener.loadUncheckedMemes();
+            registerCommands();
+        }
     }
 
-    public static String tryInit(){
+    public static String getToken(){
         String token = null;
         try {
             Dotenv dotenv = Dotenv.configure()
@@ -51,14 +92,11 @@ public class Main {
                     .load();
             token = dotenv.get("ZERSTBOT_TOKEN");
         }catch (DotenvException e) {
-            log("[Error] Dotenv loading failed: " + e.getMessage(), true);
+            LOGGER.error("[Error] Dotenv loading failed {}", e.getMessage());
         }
         return token;
     }
 
-    public static void log(String message, boolean logToFile) {
-        System.out.println(message);
-    }
 
     public static String getJarDirectory() {
         try {
@@ -75,14 +113,30 @@ public class Main {
         }
     }
 
-    public void loadConfig() {
-        Gson gson = new Gson();
-
-        try (FileReader reader = new FileReader("config.json")) {
-            config = gson.fromJson(reader, Config.class);
-        }catch (IOException e) {
-            log("[Error] Error loading config file: " + e.getMessage(), true);
+    private static Button getUpButton(){
+        Emoji upVoteEmoji = Emoji.fromFormatted("⬛");
+        try {
+            upVoteEmoji = Emoji.fromFormatted(config.getUpvoteEmojiCode());
+        }catch (IllegalArgumentException e){
+            LOGGER.error("Could not retrieve upvote emoji from config.json: {}", e.getMessage());
         }
+
+        return Button.of(ButtonStyle.SECONDARY, "upButton", "0", upVoteEmoji);
     }
+
+    private static void registerCommands(){
+        CommandData leaderboardCommand = net.dv8tion.jda.api.interactions.commands.build.Commands.slash("top", "Muestra el top de memes")
+                .addOptions(new OptionData(OptionType.STRING, "periodo", "Periodo de tiempo", true)
+                        .addChoice("Semanal", "weekly")
+                        .addChoice("Todo el tiempo", "alltime"));
+
+        CommandData resetCooldownCommand = net.dv8tion.jda.api.interactions.commands.build.Commands.slash("cd", "Resetea el cooldown para enviar un meme de un usuario")
+                .addOption(OptionType.USER, "usuario", "El usuario a resetear", true)
+                .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MODERATE_MEMBERS));
+
+        jda.updateCommands().addCommands(leaderboardCommand, resetCooldownCommand).queue();
+    }
+
+
 
 }
---
+++ src/main/java/me/rama/bot/MessageListener.java
@@ -0,0 +1,267 @@
+package me.rama.bot;
+
+import me.rama.Config;
+import me.rama.Meme;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.components.actionrow.ActionRow;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.*;
+import java.sql.SQLException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
+
+import static me.rama.Main.*;
+
+
+public class MessageListener extends ListenerAdapter {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(MessageListener.class);
+
+    private final Map<Long, Boolean> cooldownOverrides = new ConcurrentHashMap<>();
+
+
+    private final Config config;
+    private TextChannel memeChannel;
+    private Guild guild;
+
+    public MessageListener(Config config){
+        this.config = config;
+    }
+
+    public void init(){
+        memeChannel = jda.getTextChannelById(config.getMemeChannelId());
+        if(memeChannel == null){
+            LOGGER.error("Could not retrieve meme channel.");
+        }
+
+        guild = jda.getGuildById(config.getGuildID());
+        if(guild == null){
+            LOGGER.warn("Main guild not found.");
+        }
+
+
+    }
+
+
+    public void onMessageReceived(MessageReceivedEvent event) {
+
+        User sender = event.getAuthor();
+
+        if(!sender.getId().equals(event.getJDA().getSelfUser().getId())) { // 1514631312437280859 = bot ID
+
+            if (memeChannel != null) {
+                if (event.getChannel().getId().equals(memeChannel.getId())) {
+                    handleMessage(event.getChannel(), event.getMessage(), sender);
+                }
+            }
+        }
+
+    }
+
+    private void handleMessage(MessageChannel channel, Message message, User author){
+        channel.sendTyping().queue();
+        ParsedMessage pMessage = new ParsedMessage(message);
+        if(pMessage.hasUrl()) {
+
+            boolean canSend = checkCD(author.getIdLong(), channel);
+            if(!canSend){
+                message.delete().queue();
+            }else {
+
+                MessageEmbed embed = buildURLEmbed(pMessage);
+                memeChannel.sendMessageEmbeds(embed)
+                        .setComponents(ActionRow.of(upButton))
+                        .queue(s -> {
+                            Meme meme = new Meme(s.getId(), author.getId(), 0);
+                            try {
+                                db.saveMeme(meme);
+                                db.setLastProcessedId(s.getId());
+                            } catch (SQLException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+                message.delete().queue();
+            }
+        } else if(pMessage.hasAttachedImage()){
+
+            boolean canSend = checkCD(author.getIdLong(), channel);
+            if(!canSend) {
+                message.delete().queue();
+            }else {
+
+                long maxSize = 10000000L;
+                Message.Attachment attachment = pMessage.getFirstAttachment();
+                try {
+                    maxSize = guild.getBoostTier().getMaxFileSize();
+                } catch (NullPointerException e) {
+                    LOGGER.error("Could not retrieve guild boost tier because guild is null. Defaulting to 10MB");
+                }
+
+                if (attachment.getSize() > maxSize) {
+
+                    message.delete().queue();
+                    channel.sendMessage(author.getAsMention() + "<:pepow:1280353071267971174> La imagen/gif que subiste pesa demasiado!"
+                    ).queue(msg -> msg.delete().queueAfter(3, TimeUnit.SECONDS));
+                    return;
+                }
+
+
+                pMessage.captureImage()
+                        .thenAccept(pm -> {
+                            MessageEmbed embed = buildImageEmbed(pm);
+                            memeChannel.sendMessageEmbeds(embed)
+                                    .setComponents(ActionRow.of(upButton))
+                                    .addFiles(pm.toFileUpload())
+                                    .queue(s -> {
+                                        Meme meme = new Meme(s.getId(), author.getId(), 0);
+                                        try {
+                                            db.saveMeme(meme);
+                                            db.setLastProcessedId(s.getId());
+                                        } catch (SQLException e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                        message.delete().queue();
+                                    });
+                        })
+                        .exceptionally(ex -> {
+                            LOGGER.error("Error capturing image", ex);
+                            return null;
+                        });
+            }
+        }else {
+
+            message.delete().queue();
+            channel.sendMessage(author.getAsMention() + " Este canal es solo para memes <:pepow:1280353071267971174>").queue(msg -> msg.delete().queueAfter(3, TimeUnit.SECONDS));
+        }
+
+    }
+
+    private MessageEmbed buildURLEmbed(ParsedMessage message) {
+        String description = message.getText();
+        User author = message.getAuthor();
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setAuthor(author.getEffectiveName(), null, author.getAvatarUrl());
+        eb.setTitle(description);
+        eb.setDescription(message.getUrl());
+        eb.setTimestamp(Instant.now());
+        eb.setColor(Color.YELLOW);
+        return eb.build();
+    }
+
+    private MessageEmbed buildImageEmbed(ParsedMessage message) {
+        String description = message.getText();
+        User author = message.getAuthor();
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setAuthor(author.getEffectiveName(), null, author.getAvatarUrl());
+        eb.setTitle(description);
+
+        eb.setImage("attachment://" + message.getImageFileName());
+
+        eb.setTimestamp(Instant.now());
+        eb.setColor(Color.YELLOW);
+        return eb.build();
+    }
+
+    public void loadUncheckedMemes(){
+        TextChannel memeChannel = jda.getTextChannelById(config.getMemeChannelId());
+        long lastMessageProcessedId = 0;
+        try {
+            lastMessageProcessedId = db.getLastProcessedId();
+        }catch(SQLException | NullPointerException e){
+            LOGGER.error("Could not retrieve last processed id from database {}", e.getMessage());
+        }
+
+        if(memeChannel != null){
+
+            if(lastMessageProcessedId == 0){
+                LOGGER.info("Last processed id is 0");
+            }else {
+
+                memeChannel.getHistoryAfter(lastMessageProcessedId, 100).queue(messages -> {
+
+                    List<Message> history = messages.getRetrievedHistory();
+
+                    LOGGER.info("Loading {} unchecked messages from last time.", history.size());
+
+                    for (Message message : history) {
+
+                        if(!message.getAuthor().getId().equals("1514631312437280859")) {
+                            handleMessage(memeChannel, message, message.getAuthor());
+                        }
+                    }
+                });
+            }
+
+        }
+    }
+
+    private boolean checkCD(Long authorId, MessageChannel channel){
+        boolean b = false;
+        boolean hasOverride = cooldownOverrides.getOrDefault(authorId, false);
+        if(hasOverride){
+            return true;
+        }
+        try {
+            Long lastTimestamp = db.getLastMemeTimestamp(authorId);
+
+            if (lastTimestamp != null) {
+                Duration cooldown = Duration.ofSeconds(config.getMemeCooldownSeconds());
+                Instant lastPost = Instant.ofEpochMilli(lastTimestamp);
+                Duration elapsed = Duration.between(lastPost, Instant.now());
+
+                if (elapsed.compareTo(cooldown) < 0) {
+
+                    Duration remaining = cooldown.minus(elapsed);
+
+                    channel.sendMessage("Todavía no podés postear otro meme. Esperá "
+                                    + formatDuration(remaining) + " <:pepow:1280353071267971174>")
+                            .queue(m -> m.delete().queueAfter(3, TimeUnit.SECONDS));
+
+                }else{
+                    b = true;
+                }
+            }
+        } catch (SQLException e) {
+            LOGGER.error("Could not retrieve last meme timestamp from database {}", e.getMessage());
+        }
+
+        return b;
+    }
+
+    private String formatDuration(Duration duration) {
+        long days = duration.toDays();
+        long hours = duration.toHoursPart();
+        long minutes = duration.toMinutesPart();
+
+        if (days > 0) {
+            return days + " día/s " + hours + " hora/s";
+        } else if (hours > 0) {
+            return hours + " hora/s " + minutes + " minuto/s";
+        } else {
+            return minutes + " minuto/s";
+        }
+    }
+
+
+    public void putOverride(String id, boolean b) {
+
+        cooldownOverrides.put(Long.parseLong(id), b);
+
+    }
+}
